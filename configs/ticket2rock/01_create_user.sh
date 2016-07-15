d528993fe85d737033adf2c2a63d2c0eea00b434
Erhard Siegl <erhard.siegl@gepardec.com>
2016-07-15 09:37:55 +0200

    test-user Password changed

--- configs/ticket2rock/01_create_user.sh
+++ configs/ticket2rock/01_create_user.sh
@@ -1,7 +1,7 @@
 RoleFile=$JBOSS_HOME/standalone/configuration/application-roles.properties
 
 $JBOSS_HOME/bin/add-user.sh -u admin -p jboss@123 -r ManagementRealm
-$JBOSS_HOME/bin/add-user.sh -a -u guest -p guest@123  -r ApplicationRealm -g guest
+$JBOSS_HOME/bin/add-user.sh -a -u guest -p guest -r ApplicationRealm -g guest
 
 #sed -i 's/^.*guest=.*/guest=guest/' $RoleFile
 #sed -i 's/^.*guest=/#&/' $RoleFile
